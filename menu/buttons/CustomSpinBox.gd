15159a164afca4c1d1839c20a796a8853c057388
Hashtop <heitorofonseca@gmail.com>
2021-09-22 19:04:05 -0300

    Adiciona a rolagem automática (após ficar um certo período segurando) na spinbox.

--- menu/buttons/CustomSpinBox.gd
+++ menu/buttons/CustomSpinBox.gd
@@ -9,6 +9,8 @@ export var value := 0
 export var min_value := 0
 export var max_value := 0
 
+export var cooldown_time := 0.8
+
 onready var a1_button = $CenterContainer/HBoxContainer/Arrow1/Arrow/Button
 onready var a2_button = $CenterContainer/HBoxContainer/Arrow2/Arrow/Button
 onready var lefta_spr = $CenterContainer/HBoxContainer/Arrow1/Arrow/Sprite
@@ -19,6 +21,9 @@ onready var tween = $Tween
 
 onready var parent = get_parent()
 
+var still_pressed = false
+var grace_period = 1
+
 func _ready():
 	if value < min_value:
 		value = min_value
@@ -26,7 +31,23 @@ func _ready():
 	a1_button.connect("pressed", self, "subtract_value")
 	a2_button.connect("pressed", self, "add_value")
 	
+	a1_button.connect("button_up", self, "stop_pressing")
+	a2_button.connect("button_up", self, "stop_pressing")
+	
 	update()
+
+func _process(delta):
+	if (still_pressed == true):
+		if (a1_button.pressed == true):
+			subtract_value()
+		elif (a2_button.pressed == true):
+			add_value()
+	
+	
+	if (tween.is_active() == false):
+		lefta_spr.position = Vector2(0, 0)
+		righta_spr.position = Vector2(0, 0)
+
 
 func subtract_value():
 	if value - 1 >= min_value:
@@ -37,11 +58,23 @@ func subtract_value():
 		update()
 		
 		tween.interpolate_property(lefta_spr, "position", 
-				Vector2(0, 0), Vector2(-20, 0), 0.1,Tween.TRANS_EXPO, Tween.EASE_OUT)
+				Vector2(0, 0), Vector2(-10, 0), 0.1,Tween.TRANS_EXPO, Tween.EASE_OUT)
 		tween.start()
-		yield(tween, "tween_completed")
 		
-		lefta_spr.position = Vector2(0, 0)
+		if (still_pressed == true):
+			var timer = create_timer(cooldown_time)
+			
+			yield(timer, "timeout")
+			timer.remove_from_group("active_timers")
+			timer.queue_free()
+		else:
+			var timer = create_timer(grace_period)
+			
+			yield(timer, "timeout")
+			timer.remove_from_group("active_timers")
+			timer.queue_free()
+			
+			still_pressed = true
 
 
 func add_value():
@@ -53,14 +86,49 @@ func add_value():
 		update()
 		
 		tween.interpolate_property(righta_spr, "position", 
-				Vector2(0, 0), Vector2(20, 0), 0.1, Tween.TRANS_EXPO, Tween.EASE_OUT)
+				Vector2(0, 0), Vector2(10, 0), 0.1, Tween.TRANS_EXPO, Tween.EASE_OUT)
 		tween.start()
-		yield(tween, "tween_completed")
 		
-		righta_spr.position = Vector2(0, 0)
-		
+		if (still_pressed == true):
+			var timer = create_timer(cooldown_time)
+			
+			yield(timer, "timeout")
+			timer.remove_from_group("active_timers")
+			timer.queue_free()
+		else:
+			var timer = create_timer(grace_period)
+			
+			yield(timer, "timeout")
+			timer.remove_from_group("active_timers")
+			timer.queue_free()
+			
+			still_pressed = true
+
+
+func stop_pressing():
+	var active_timers = get_tree().get_nodes_in_group("active_timers")
+	
+	if (active_timers.size() != 0):
+		for current_timer in active_timers:
+			current_timer.queue_free()
+	
+	still_pressed = false
+	
+
 
 func update():
 	text.text = str(value)
 	
 	emit_signal("value_changed", value)
+
+
+func create_timer(wait_time) -> Node:
+	var cooldown = Timer.new()
+	add_child(cooldown)
+	
+	cooldown.wait_time = wait_time
+	cooldown.start()
+	
+	cooldown.add_to_group("active_timers")
+	
+	return cooldown
